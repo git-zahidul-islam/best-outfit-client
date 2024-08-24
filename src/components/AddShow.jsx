9fab2aa92a53fd2fba5a39e14116f1bb617b9720
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-08-24 23:47:14 +0600

    add: add a ad from adsterra

--- src/components/AddShow.jsx
+++ src/components/AddShow.jsx
@@ -1,23 +1,70 @@
-import { useEffect } from "react";
-import { loadAd } from "./ad";
+import { useEffect, useRef } from "react";
 
-const AddShow = () => {
-    useEffect(() => {
-        loadAd(); 
+export default function AddShow() {
+  const banner = useRef(null);
 
-        return () => {
-          const adDiv = document.getElementById('ad-container');
-          if (adDiv && adDiv.firstChild) {
-              adDiv.removeChild(adDiv.firstChild);
-          }
-        };
-    }, []);
+  useEffect(() => {
+    // Function to update ad size based on screen width
+    function updateAdSize() {
+      let width, height;
 
-    return (
-        <div className="md:h-20 h-14 md:w-[400px] w-60 my-5 mx-auto">
-            
-        </div>
-    );
-};
+      if (window.innerWidth < 640) {
+        // Small devices
+        width = window.innerWidth * 0.9; // 90% of the screen width
+        height = 50; // Fixed height
+      } else {
+        // Larger devices
+        width = 320; // Default width
+        height = 50; // Fixed height
+      }
 
-export default AddShow;
+      if (banner.current) {
+        banner.current.style.width = `${width}px`;
+        banner.current.style.height = `${height}px`;
+      }
+    }
+
+    updateAdSize(); // Initial size adjustment
+    window.addEventListener("resize", updateAdSize); // Adjust on resize
+
+    if (banner.current && !banner.current.firstChild) {
+      const confScript = document.createElement("script");
+      const mainScript = document.createElement("script");
+
+      const atOptions = {
+        key: "6f257b46bf5627156750307c9d72227d",
+        format: "iframe",
+        height: 50, // Fixed height
+        width: banner.current.style.width.replace('px', ''), // Dynamic width
+        params: {},
+      };
+
+      confScript.type = "text/javascript";
+      confScript.innerHTML = `atOptions = ${JSON.stringify(atOptions)};`;
+
+      mainScript.type = "text/javascript";
+      mainScript.src = `//www.topcreativeformat.com/${atOptions.key}/invoke.js`;
+
+      banner.current.appendChild(confScript);
+      banner.current.appendChild(mainScript);
+    }
+
+    return () => {
+      window.removeEventListener("resize", updateAdSize);
+    };
+  }, []);
+
+  return (
+    <div
+      className="my-5 mx-auto"
+      ref={banner}
+      style={{
+        maxWidth: "100%",
+        height: "50px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></div>
+  );
+}
